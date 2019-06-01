87f8ab8c94ec9bc789819374b050ed973274529e
priyasindhe <49157296+priyasindhe@users.noreply.github.com>
2019-06-01 10:43:04 +0530

    Test code pushed to repository.

---
+++ Area square/Main.java
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int side,area;
+  scanf("%d%d",&side,&area);
+  area=side*side;
+  printf("%d",area);
+  return 0;
+}
---
+++ Average of three numbers/Main.java
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a=1,b=2,c=3,sum,average;
+  scanf("%d%d%d",&a,&b,&c);
+  sum =a+b+c;
+  average =sum/3;
+  printf("%d",average);
+  
+  return 0;
+}
